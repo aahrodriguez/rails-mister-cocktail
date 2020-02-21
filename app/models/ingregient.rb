0f0d3fc356469460d0896f563f9c054763b23d73
Andres Hernandez <aahrodriguez@gmail.com>
2020-02-21 13:41:44 -0300

    Models and validation

--- app/models/ingregient.rb
+++
@@ -1,2 +0,0 @@
-class Ingregient < ApplicationRecord
-end
--- db/migrate/20200220182351_create_ingregients.rb
+++
@@ -1,9 +0,0 @@
-class CreateIngregients < ActiveRecord::Migration[5.2]
-  def change
-    create_table :ingregients do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
